16ad96387746ccfd4909ce4ba09a1b020cf97b07
kentoverse <kentoverse@gmail.com>
2025-11-25 03:19:03 -0500

    dsds

--- botworks/app/index.tsx
+++ botworks/app/index.tsx
@@ -2,12 +2,12 @@ import { View, Text, Pressable } from 'react-native';
 import { router } from 'expo-router';
 
 export default function IndexPage() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>My Home Page</Text>
-      <Pressable onPress={() => router.push('/modal')}>
-        <Text>Go to Modal</Text>
-      </Pressable>
-    </View>
-  );
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>My Home Page</Text>
+            <Pressable onPress={() => router.push('/modal')}>
+                <Text>Go to Modal</Text>
+            </Pressable>
+        </View>
+    );
 }
